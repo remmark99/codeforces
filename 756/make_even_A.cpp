66177e074d67f4fc5e2ccb4c1b7c8ee89c6c1523
mysterio <mysterio@student.21-school.ru>
2023-03-17 22:13:32 +0300

    final solution

--- 756/make_even_A.cpp
+++ 756/make_even_A.cpp
@@ -1,3 +1,35 @@
+/*polycarp has an integer n
+
+that doesn't contain the digit 0. He can do the following operation with his
+number several (possibly zero) times:
+
+    Reverse the prefix of length l
+
+(in other words, l leftmost digits) of n. So, the leftmost digit is swapped
+with the l-th digit from the left, the second digit from the left swapped
+with (l−1)-th left, etc. For example, if n=123456789 and l=5, then the new
+value of n will be 543216789
+
+    .
+
+Note that for different operations, the values of l
+can be different. The number l can be equal to the length of the number n —
+in this case, the whole number n
+
+is reversed.
+
+Polycarp loves even numbers. Therefore, he wants to make his number even. At
+the same time, Polycarp is very impatient. He wants to do as few operations
+as possible.
+
+Help Polycarp. Determine the minimum number of operations he needs to perform
+with the number n
+
+to make it even or determine that this is impossible.
+
+You need to answer t
+independent test cases.*/
+
 #include <cstring>
 #include <iostream>
 #include <string>
@@ -28,3 +60,4 @@ int main(int argc, char *argv[]) {
 
   return 0;
 }
+
