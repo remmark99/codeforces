3700c6b8006b47870753ea1af3e70431a4437230
vi <cleopsol@student.21-school.ru>
2023-03-21 21:49:41 +0500

    835

---
+++ 835/A_medium_number.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  int tt;
+  cin >> tt;
+
+  while (tt--) {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int max
+  }
+
+  return 0;
+}
