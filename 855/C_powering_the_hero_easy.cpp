80a175737d2a8aad505b1c73f87f598a40ebc7e5
Vi <victor.dosca@yandex.ru>
2023-03-24 16:43:06 +0500

    powering the hero start

---
+++ 855/C_powering_the_hero_easy.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+
+void solve() {
+  int n;
+  cin >> n;
+
+  multiset<int> ms;
+
+  uint64_t res = 0;
+  for (int i = 0; i < n; i++) {
+    int temp;
+    cin >> temp;
+
+    if (temp == 0) {
+      if (ms.size() == 0) {
+        continue;
+      } else {
+        ms.
+      }
+    }
+  }
+}
+
+int main(int argc, char *argv[]) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+
+  int tt;
+  cin >> tt;
+
+  while (tt--) {
+    solve();
+  }
+
+  return 0;
+}
